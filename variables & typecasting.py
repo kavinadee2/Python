fe6d9ae823daa73320e2df33363e173de5f6b07b
kavinadee2 <kavinduwick02@gmail.com>
2025-10-05 09:45:16 +0530

    add var and typecasting

---
+++ variables & typecasting.py
@@ -0,0 +1,25 @@
+#variable= it contain values(integers,strings,floats,booleans)
+
+first_name="Kavindu"
+last_name="Nadeeshana"
+age=23
+gpa=3.97
+email="kavinduwick02@gmail.com"
+is_Student=True
+
+
+print(f"✨ Hello im {first_name} {last_name}.im {age} year's old.\nmy university is NSBM Green University.\nim cs student.my gpa is {gpa}.im always the best.\nif you want contact me - {email}.  ")
+print(f"is student = {is_Student} ")
+
+
+#type
+str()#any variable convert to string.
+int()#any variable convert to integer.
+bool()#any variable convert to boolean.
+float()#any variable convert to float.
+
+#if you want to checktype of variable do this
+ #print(type("varible name"))
+#print(type(gpa))
+age=str(age)
+print(type(age))
